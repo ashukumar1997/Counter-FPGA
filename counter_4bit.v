f1b02ef5f170a0c055c247877f4e608947bc462e
Ashutosh Kumar <89641410+ashukumar1997@users.noreply.github.com>
2021-08-27 18:05:34 +0530

    Update counter_4bit.v

--- counter_4bit.v
+++ counter_4bit.v
@@ -18,11 +18,35 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module counter_4b(a); // Port Information
-output reg [3:0]a;
-initial
+module counter_4b(clk,rst,count); // Port Information
+input clk,rst;  //Input Ports
+output reg [3:0] count;  //Output Ports
+reg [25:0]temp;   //Temporary variables
+reg clk_div;
+
+always@(posedge clk)
 begin
-a <= #10 4'd4;
-a <= #20 4'd8;
+if(!rst)
+begin
+temp=0;
+clk_div<='d0;
+count <= 'd0;
 end
+else
+begin
+
+temp<=temp+26'd1;
+clk_div<=temp[20];
+end
+end
+
+always@(posedge clk_div)
+begin
+if(!rst)
+count<=4'd0;
+else
+   count<=count+4'd1;
+end
+
+
 endmodule
